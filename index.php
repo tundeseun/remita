bb514b7bb14ca6412b25c5b400dca51d13cc4ec4
pgcollege <tunde.abiona@pgcollege.ui.edu.ng>
2024-09-28 22:17:26 +0100

    Add cpanel.yml for automatic deployment

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <h> hello </h1><br>
     <p>Test Git</p>
+    <p>SAN is an independent scientific Association in Nigeria, dedicated to promoting excellence in science. One of the
+        major activities of SAN is the annual Conference. The annual conference is host by different public, private
+        institutions of higher learning and research institutes holds at different locations across the nation. The
+        conference avails scientists both at local and international level the opportunity to interact, network, present
+        papers and share ideas on recent issues of interest in the scientific clime. Communique of such meetings are
+        published in the national dailies. Also, Proceedings </p>
